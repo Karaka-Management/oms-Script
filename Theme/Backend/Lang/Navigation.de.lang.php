885681c83a9fbed3f787c1567ec4636965a0c370
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-03-22 18:42:34 +0100

    implement localization

---
+++ Theme/Backend/Lang/Navigation.de.lang.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.4
+ *
+ * @package   Modules\Helper
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+return ['Navigation' => [
+    'Create'         => 'Erstellt',
+    'CreateReport'   => 'Erstelle Report',
+    'CreateTemplate' => 'Erstelle Template',
+    'Helper'         => 'Helper',
+    'List'           => 'Liste',
+    'Reporting'      => 'Reporting',
+]];
---
+++ Theme/Backend/Lang/de.lang.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.4
+ *
+ * @package   Modules\Helper
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+return ['Helper' => [
+    'Add'              => 'Hinzufügen',
+    'CentralizedDB'    => 'Zentrale DB',
+    'CentralizedFiles' => 'Zentrale Datei',
+    'Created'          => 'Erstellt',
+    'CreatedBy'        => 'Erstellt von',
+    'Creator'          => 'Ersteller',
+    'Data'             => 'Daten',
+    'Database'         => 'Datenbank',
+    'Dataset'          => 'Daten',
+    'Datasets'         => 'Daten',
+    'Description'      => 'Beschreibung',
+    'Edit'             => 'Bearbeiten',
+    'Expected'         => 'Erwartet',
+    'Export'           => 'Export',
+    'FileNames'        => 'Dateinamen',
+    'Files'            => 'Dateien',
+    'Helper'           => 'Helper',
+    'Helpers'          => 'Helpers',
+    'IndividualDB'     => 'Einzelne DB',
+    'Info'             => 'Info',
+    'Language'         => 'Sprache',
+    'Load'             => 'Laden',
+    'Media'            => 'Media',
+    'MediaDirectory'   => 'Media Ordner',
+    'Modified'         => 'Bearbeitet',
+    'Name'             => 'Name',
+    'New'              => 'Neu',
+    'Other'            => 'Sonstige',
+    'Overview'         => 'Übersicht',
+    'Permission'       => 'Rechte',
+    'Print'            => 'Drucken',
+    'Report'           => 'Bericht',
+    'Reports'          => 'Berichte',
+    'Select'           => 'Auswahl',
+    'Settings'         => 'Einstellungen',
+    'Source'           => 'Quelle',
+    'Sources'          => 'Quelle',
+    'Standalone'       => 'Eigenständig',
+    'Storage'          => 'Speicher',
+    'Template'         => 'Template',
+    'Title'            => 'Titel',
+    'Type'             => 'Typ',
+    'Updated'          => 'Bearbeitet',
+]];
