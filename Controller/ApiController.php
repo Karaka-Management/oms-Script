ca4297af14fe3403e120526a196f0cad38f5d666
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-12-16 21:59:21 +0100

    fix url

--- Controller/ApiController.php
+++ Controller/ApiController.php
@@ -8,7 +8,7 @@
  * @copyright Dennis Eichhorn
  * @license   OMS License 1.0
  * @version   1.0.0
- * @link      https://karaka.app
+ * @link      https://jingga.app
  */
 declare(strict_types=1);
 
@@ -50,7 +50,7 @@ use phpOMS\Views\View;
  *
  * @package    Modules\Helper
  * @license    OMS License 1.0
- * @link       https://karaka.app
+ * @link       https://jingga.app
  * @since      1.0.0
  */
 final class ApiController extends Controller
