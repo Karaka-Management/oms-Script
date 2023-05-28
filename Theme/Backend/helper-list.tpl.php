5354b65e6cb86d102608786aa0830176eaab0a1e
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-05-28 01:13:22 +0000

    style fixes, bug fixes

--- Theme/Backend/helper-list.tpl.php
+++ Theme/Backend/helper-list.tpl.php
@@ -68,7 +68,7 @@ echo $this->getData('nav')->render(); ?>
 <div class="row">
     <div class="col-xs-12">
         <div class="portlet">
-            <div class="portlet-head"><?= $this->getHtml('Helpers'); ?><i class="fa fa-download floatRight download btn"></i></div>
+            <div class="portlet-head"><?= $this->getHtml('Helpers'); ?><i class="lni lni-download download btn end-xs"></i></div>
             <div class="slider">
             <table id="helperList" class="default sticky">
                 <thead>
