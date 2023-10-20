c27d11d0bade4247dcc8fa5a20f2993f95575953
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-20 13:08:56 +0000

    icon fixes, loading optimizations

--- Theme/Backend/helper-single.tpl.php
+++ Theme/Backend/helper-single.tpl.php
@@ -154,7 +154,7 @@ echo $this->data['nav']->render(); ?>
                         <td><?= $this->getHtml('Tags'); ?>
                         <td>
                             <?php $tags = $template->getTags(); foreach ($tags as $tag) : ?>
-                                <span class="tag" style="background: <?= $this->printHtml($tag->color); ?>"><?= empty($tag->icon) ? '' : '<i class="' . $this->printHtml($tag->icon) . '"></i>'; ?><?= $this->printHtml($tag->getL11n()); ?></span>
+                                <span class="tag" style="background: <?= $this->printHtml($tag->color); ?>"><?= empty($tag->icon) ? '' : '<i class="g-icon">' . $this->printHtml($tag->icon) . '</i>'; ?><?= $this->printHtml($tag->getL11n()); ?></span>
                             <?php endforeach; ?>
                 </table>
             </div>
